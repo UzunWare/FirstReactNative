102e0b8a48b20fef925053765e034ac220127617
UzunWare <emrekrkmzpk@gmail.com>
2023-08-20 17:06:38 -0500

    First Commit

---
+++ src/screens/ComponentScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Text, StyleSheet, View} from 'react-native';
+
+const ComponentsScreen = () => {
+    const greeting = <Text>Hi there</Text>;
+
+    return (
+    <View>
+        <Text style={styles.textStyle}>This is the component screen</Text>
+        {greeting}
+    </View>)
+};
+
+const styles = StyleSheet.create({
+    textStyle: {
+        fontSize: 60
+    }
+});
+
+export default ComponentsScreen;
